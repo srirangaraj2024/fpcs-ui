2e768bfb2157517c57e51c547bdab135c8318998
srirangaraj2024 <157279558+srirangaraj2024@users.noreply.github.com>
2024-01-25 21:47:20 +0530

    samle emp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ListEmpComponent from './components/ListEmpComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to cloud web application
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ListEmpComponent></ListEmpComponent>
     </div>
   );
 }
