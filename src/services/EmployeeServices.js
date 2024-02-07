ffe96a9d09a0f1c6080e02d614af088a9588cee7
srirangaraj2024 <157279558+srirangaraj2024@users.noreply.github.com>
2024-02-07 09:53:21 +0530

    AWS changes
    AWS changes

--- src/services/EmployeeServices.js
+++ src/services/EmployeeServices.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 //const EMPLOYEE_API_BASE_URL = "http://fpcsdemo.ap-south-1.elasticbeanstalk.com/fpcs/leaves/userLeavesList?userId=abc123";
-const EMPLOYEE_API_BASE_URL = "http://localhost:5000/fpcs/employee/getEmpList";
-const CLAIM_API_BASE_URL = "http://localhost:5000/fpcs/claims/preClaimFund";
+const EMPLOYEE_API_BASE_URL = "http://fpcsdev.ap-southeast-1.elasticbeanstalk.com/fpcs/employee/getEmpList";
+const CLAIM_API_BASE_URL = "http://fpcsdev.ap-southeast-1.elasticbeanstalk.com/fpcs/claims/preClaimFund";
 class EmployeeService {
 
     getEmployees(){
